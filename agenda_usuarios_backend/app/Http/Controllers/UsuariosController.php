0214cfbc45ac536d59782940ca526f81d47dba8b
JeremySO202 <jeremyserracin@gmail.com>
2021-11-08 16:27:27 -0600

    Implementacion de IMG y Pruebas
    Insertar imágenes en la BD y pruebas de conexión

--- agenda_usuarios_backend/app/Http/Controllers/UsuariosController.php
+++ agenda_usuarios_backend/app/Http/Controllers/UsuariosController.php
@@ -7,18 +7,46 @@ use Illuminate\Http\Request;
 
 class UsuariosController extends Controller
 {
-    public function store()
+    public function index()
     {
-        $attributes = request()->validate([
-            'nom' => 'required|max:255',
-            'ced' => 'required|min:9|max:20',
-            'num' => 'required|max:15',
-            'cor' => 'required|email|max:255',
-            'dir' => 'required|max:255',
-            'fec_nac' => 'required|max:255',
-        ]);
+        $usuario = usuarios::all();
+    }
 
-        Usuarios::create($attributes);
-        return redirect('/');
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        if($request->hasfile('fot_per')){
+            $file = $request->file('fot_per');
+            $nom = time().$file->getClientOriginalName();
+            $file->move(public_path().'/fot_per/', $nom);
+        }
+
+        $usuario = new usuarios();
+        $usuario->nom = $request->input('nom');
+        $usuario->ced = $request->input('ced');
+        $usuario->num = $request->input('num');
+        $usuario->cor = $request->input('cor');
+        $usuario->dir = $request->input('dir');
+        $usuario->fec_nac = $request->input('fec_nac');
+        $usuario->fot_per = $nom;
+        $usuario->save();
+
+        // $attributes = request()->validate([
+        //     'nom' => 'required|max:255',
+        //     'ced' => 'required|min:9|max:20',
+        //     'num' => 'required|max:15',
+        //     'cor' => 'required|email|max:255',
+        //     'dir' => 'required|max:255',
+        //     'fec_nac' => 'required|max:255',
+        //     'fot_per' => 'required',
+        // ]);
+
+        // Usuarios::create($attributes);
+
+        return view('create');
     }
 }
--- agenda_usuarios_backend/database/migrations/2021_11_05_210450_create_usuarios_table.php
+++ agenda_usuarios_backend/database/migrations/2021_11_05_210450_create_usuarios_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsuariosTable extends Migration
 {
@@ -14,16 +15,16 @@ class CreateUsuariosTable extends Migration
     public function up()
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
+            $table->string('ced')->primary();
             $table->string('nom');
-            $table->string('ced');
             $table->string('num');
             $table->string('cor');
             $table->string('dir');
             $table->date('fec_nac');
-            $table->binary('fot_per');
+            $table->string('fot_per');
             $table->timestamps();
         });
+        // DB::statement("ALTER TABLE usuarios ADD fot_per LONGBLOB");
     }
 
     /**
