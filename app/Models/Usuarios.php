4d71d4c41175e4b608a8872a95b2a6fd5df58614
JeremySO202 <jeremyserracin@gmail.com>
2021-11-12 11:18:51 -0600

    Arreglo Error Usuarios 2

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Usuarios extends Model
 {
     use HasFactory;
+    protected $table = "usuarios";
+
+    protected $guarded = [];
 }
